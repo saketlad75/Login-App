993aa83efd70d4070b7e5f8b24bbc5dcba2320bb
saketlad75 <saket.lad@spit.ac.in>
2023-09-07 15:40:51 +0530

    Updated Code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,session
 import mysql.connector
-conn = mysql.connector.connect(host="%",user="saket",password="@Svlad2305",database="login")
+conn = mysql.connector.connect(host="127.0.0.1",user="root",password="@Svlad2305",database="login",auth_plugin='mysql_native_password')
 cursor = conn.cursor()
 app = Flask(__name__)
 
@@ -15,12 +15,33 @@ def reg():
 def login_validation():
     email = request.form.get('email')
     password = request.form.get('password')
-    cursor.execute("""SELECT * FROM `users` WHERE `email` LIKE{} AND `password` LIKE {}""".format(email,password))
+    query = "SELECT * FROM `user` WHERE `email` LIKE %s and `password` LIKE %s"
+    cursor.execute(query,(email,password))
     users = cursor.fetchall()
     #return "The email is {} and the password is {}".format(email,password )
-    return users
+    if len(users)>0:
+        return render_template('home.html')
+    else:
+        return render_template('login.html')
+    #print(users)
+
+    #return "Hello"
 #@app.route('/home')
 #def home():
 #    return render_template('home.html')
+@app.route('/add_user',methods=['POST'])
+def add_user():
+    name = request.form.get('uname')
+    email = request.form.get('uemail')
+    password = request.form.get('upassword')
+
+    query = 'INSERT INTO user VALUES(3,name,email,password)'
+    cursor.execute(query)
+    conn.commit()
+    return "User registered Successfully"
+@app.route('/logout')
+def logout():
+    session.pop('user_id')
+    return redirect('/')
 if __name__ == '__main__':
     app.run(debug=True)
